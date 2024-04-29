e0b25ea8ee4d29b86e42c10301e7ab7043320ac4
samir <harchesamir007@gmail.com>
2024-04-29 14:11:26 +0100

    a

--- lib/home.dart
+++ lib/home.dart
@@ -1,62 +1,86 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:tp45/details.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:tp45/details.dart';
 
-class Home extends StatefulWidget  {
+class Note {
+  int id;
+  String content;
+  DateTime date;
+
+  Note({
+    required this.id,
+    required this.content,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'content': content,
+      'date': date.toString(),
+    };
+  }
+}
+
+class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> with WidgetsBindingObserver {
   List<Note> _notes = [];
-  Note? _selectedNote ;
+  Note? _selectedNote;
+  Database? _database;
+
+  final FlutterTts flutterTts = FlutterTts();
+
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance?.addObserver(this);
-  }
-  FlutterTts flutterTts = FlutterTts();
-
-  void speak(String text) async {
-    print('*******************************played');
-    await flutterTts.setLanguage('fr-FR');
-    await flutterTts.setPitch(1);
-    await flutterTts.speak(text);
+    _initDatabase();
   }
 
-  void stop() async {
-    await flutterTts.stop();
+  Future<void> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = documentsDirectory.path + 'notes.db';
+    _database = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute('''
+        CREATE TABLE notes (
+          id INTEGER PRIMARY KEY,
+          content TEXT NOT NULL,
+          date TEXT NOT NULL
+        )
+      ''');
+        });
+    _notes = await _getAllNotes();
   }
 
-  @override
-  void dispose() {
-    stop();
-    WidgetsBinding.instance?.removeObserver(this);
-    super.dispose();
+  Future<List<Note>> _getAllNotes() async {
+    final List<Map<String, dynamic>> maps = await _database!.query('notes');
+    return List.generate(maps.length, (i) {
+      return Note(
+        id: maps[i]['id'],
+        content: maps[i]['content'],
+        date: DateTime.parse(maps[i]['date']),
+      );
+    });
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-    if (state == AppLifecycleState.paused || state == AppLifecycleState.inactive) {
-      print('**********************************************************stopped');
-     stop();
-    }
-  }
+  void _addNote() async {
+    String noteContent = '';
 
-
-
-
-
-  void _addNote() {
-    showDialog(
+    await showDialog(
       context: context,
       builder: (BuildContext context) {
-        String noteContent = '';
-
         return AlertDialog(
-
           title: Text('Add Note'),
           content: TextField(
             onChanged: (value) {
@@ -67,13 +91,10 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
           actions: <Widget>[
             TextButton(
               onPressed: () {
-                setState(() {
-                  _notes.add(Note(
-                    content: noteContent,
-                    date: DateTime.now(),
-                  ));
-                });
                 Navigator.of(context).pop();
+                if (noteContent.isNotEmpty) {
+                  _insertNote(noteContent);
+                }
               },
               child: Text('Confirm'),
             ),
@@ -82,6 +103,18 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
       },
     );
   }
+
+  void _insertNote(String content) async {
+    await _database!.insert(
+      'notes',
+      Note(id: DateTime.now().millisecondsSinceEpoch, content: content, date: DateTime.now()).toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    setState(() {
+      _notes.add(Note(id: DateTime.now().millisecondsSinceEpoch, content: content, date: DateTime.now()));
+    });
+  }
+
 
   void _viewNoteDetails(Note note) {
     Navigator.push(
@@ -96,14 +129,15 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
   }
 
 
+
   void _viewNoteDetails2(Note note) {
     setState(() {
       _selectedNote = note;
     });
   }
 
-  void _confirmDelete(BuildContext context, int index) {
-    showDialog(
+  void _confirmDelete(BuildContext context, int id) async {
+    await showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
@@ -111,10 +145,15 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
           content: Text('Are you sure you want to delete this note?'),
           actions: <Widget>[
             TextButton(
-              onPressed: () {
+              onPressed: () async {
+                await _database!.delete(
+                  'notes',
+                  where: 'id = ?',
+                  whereArgs: [id],
+                );
                 setState(() {
-                  _notes.removeAt(index);
-                  _selectedNote=null;
+                  _notes.removeWhere((note) => note.id == id);
+                  _selectedNote = null;
                 });
                 Navigator.of(context).pop();
               },
@@ -131,7 +170,6 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
       },
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -173,7 +211,7 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
       itemBuilder: (context, index) {
         return GestureDetector(
           onTap: () => _viewNoteDetails(_notes[index]),
-          onLongPress: () => _confirmDelete(context, index),
+          onLongPress: () => _confirmDelete(context, _notes[index].id),
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
             child: Column(
@@ -185,7 +223,6 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
                     width: 50, // Adjust as needed
                     height: 50, // Adjust as needed
                   ),
-
                   title: Text(
                     'HARCHE Samir',
                     style: TextStyle(
@@ -216,7 +253,7 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
       itemBuilder: (context, index) {
         return GestureDetector(
           onTap: () => _viewNoteDetails2(_notes[index]),
-          onLongPress: () => _confirmDelete(context, index),
+          onLongPress: () => _confirmDelete(context, _notes[index].id),
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
             child: Column(
@@ -259,62 +296,72 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
       child: _selectedNote == null
           ? Center(child: Text('No note selected'))
           : Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/logo.png',
-                  width: 50, // Adjust as needed
-                  height: 50, // Adjust as needed
-                ),
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/logo.png',
+            width: 50, // Adjust as needed
+            height: 50, // Adjust as needed
+          ),
 
-                SizedBox(height: 16.0),
-                Text(
-                  _selectedNote!.content,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Created: ${formatter.format(_selectedNote!.date)}',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.grey[600],
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-
-                      speak(_selectedNote!.content);
-                      // speak("bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ");
-
-                  },
-                  child: Text('Read Aloud'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    stop();
-                  },
-                  child: Text('stop'),
-                ),
-              ],
+          SizedBox(height: 16.0),
+          Text(
+            _selectedNote!.content,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
             ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            'Created: ${formatter.format(_selectedNote!.date)}',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.grey[600],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              speak(_selectedNote!.content);
+            },
+            child: Text('Read Aloud'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              stop();
+            },
+            child: Text('stop'),
+          ),
+        ],
+      ),
 
     );
   }
 
+  @override
+  void dispose() {
+    stop();
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
 
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.paused || state == AppLifecycleState.inactive) {
+      stop();
+    }
+  }
 
-}
+  void speak(String text) async {
+    print('*******************************played');
+    await flutterTts.setLanguage('fr-FR');
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(text);
+  }
 
-class Note {
-  String content;
-  DateTime date;
-
-  Note({
-    required this.content,
-    required this.date,
-  });
+  void stop() async {
+    await flutterTts.stop();
+  }
 }
